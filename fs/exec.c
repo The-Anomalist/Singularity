24869f979a38c0dcd2e39c7aa6d377caf5d0f74d
The_Anomalist <sellgren11@gmail.om>
2026-06-02 06:12:40 -0400

    Revert "ksu: update non-GKI KernelSU Next 3.2 legacy support"
    This reverts commit 809255f99a8f28ccd49e8b93cc89b9cd109e105a.

--- fs/exec.c
+++ fs/exec.c
@@ -1895,29 +1895,11 @@ out_ret:
 	return retval;
 }
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
-extern bool ksu_execveat_hook __read_mostly;
-extern int ksu_handle_execveat_ksud(int *fd, struct filename **filename_ptr,
-				    struct user_arg_ptr *argv,
-				    struct user_arg_ptr *envp, int *flags);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
-					void *__never_use_argv,
-					void *__never_use_envp,
-					int *__never_use_flags);
-#endif
-
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
-#ifdef CONFIG_KSU_MANUAL_HOOK
-	if (filename) {
-		if (unlikely(ksu_execveat_hook))
-			ksu_handle_execveat_ksud(&fd, &filename, &argv, &envp, &flags);
-		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
-	}
-#endif
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
 
@@ -2011,11 +1993,28 @@ void set_dumpable(struct mm_struct *mm, int value)
 	} while (cmpxchg(&mm->flags, old, new) != old);
 }
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+extern bool ksu_execveat_hook __read_mostly;
+extern __attribute__((hot, always_inline))
+int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
+			       void *__never_use_argv, void *__never_use_envp,
+			       int *__never_use_flags);
+extern int ksu_handle_execve_ksud(const char __user *filename_user,
+				  const char __user *const __user *__argv);
+#endif
+
 SYSCALL_DEFINE3(execve,
 		const char __user *, filename,
 		const char __user *const __user *, argv,
 		const char __user *const __user *, envp)
 {
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	int fd = AT_FDCWD;
+
+	ksu_handle_execve_sucompat(&fd, &filename, NULL, NULL, NULL);
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execve_ksud(filename, argv);
+#endif
 	return do_execve(getname(filename), argv, envp);
 }
 
@@ -2027,6 +2026,12 @@ SYSCALL_DEFINE5(execveat,
 {
 	int lookup_flags = (flags & AT_EMPTY_PATH) ? LOOKUP_EMPTY : 0;
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_execve_sucompat(&fd, &filename, NULL, NULL, &flags);
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execve_ksud(filename, argv);
+#endif
+
 	return do_execveat(fd,
 			   getname_flags(filename, lookup_flags, NULL),
 			   argv, envp, flags);
@@ -2037,6 +2042,12 @@ COMPAT_SYSCALL_DEFINE3(execve, const char __user *, filename,
 	const compat_uptr_t __user *, argv,
 	const compat_uptr_t __user *, envp)
 {
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	/* 32-bit su and 32-on-64 support. */
+	int fd = AT_FDCWD;
+
+	ksu_handle_execve_sucompat(&fd, &filename, NULL, NULL, NULL);
+#endif
 	return compat_do_execve(getname(filename), argv, envp);
 }
 
@@ -2047,6 +2058,10 @@ COMPAT_SYSCALL_DEFINE5(execveat, int, fd,
 		       int,  flags)
 {
 	int lookup_flags = (flags & AT_EMPTY_PATH) ? LOOKUP_EMPTY : 0;
+
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_execve_sucompat(&fd, &filename, NULL, NULL, &flags);
+#endif
 
 	return compat_do_execveat(fd,
 				  getname_flags(filename, lookup_flags, NULL),
--- fs/stat.c
+++ fs/stat.c
@@ -379,12 +379,6 @@ SYSCALL_DEFINE2(newlstat, const char __user *, filename,
 #ifdef CONFIG_KSU_MANUAL_HOOK
 extern __attribute__((hot, always_inline))
 int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
-extern void ksu_handle_newfstat_ret(unsigned int *fd,
-				    struct stat __user **statbuf_ptr);
-#if defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_COMPAT_STAT64)
-extern void ksu_handle_fstat64_ret(unsigned long *fd,
-				   struct stat64 __user **statbuf_ptr);
-#endif
 #endif
 
 #if !defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_SYS_NEWFSTATAT)
@@ -409,13 +403,8 @@ SYSCALL_DEFINE2(newfstat, unsigned int, fd, struct stat __user *, statbuf)
 	struct kstat stat;
 	int error = vfs_fstat(fd, &stat);
 
-	if (!error) {
+	if (!error)
 		error = cp_new_stat(&stat, statbuf);
-#ifdef CONFIG_KSU_MANUAL_HOOK
-		if (!error)
-			ksu_handle_newfstat_ret(&fd, &statbuf);
-#endif
-	}
 
 	return error;
 }
@@ -540,13 +529,8 @@ SYSCALL_DEFINE2(fstat64, unsigned long, fd, struct stat64 __user *, statbuf)
 	struct kstat stat;
 	int error = vfs_fstat(fd, &stat);
 
-	if (!error) {
+	if (!error)
 		error = cp_new_stat64(&stat, statbuf);
-#ifdef CONFIG_KSU_MANUAL_HOOK
-		if (!error)
-			ksu_handle_fstat64_ret(&fd, &statbuf);
-#endif
-	}
 
 	return error;
 }
